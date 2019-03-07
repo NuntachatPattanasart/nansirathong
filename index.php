4e580f7d0ea5a27d7c8c1db480929e46d3ac7433
Ploychat <macbookpro@Macbooks-MacBook-Pro-4.local>
2019-03-07 23:00:40 +0700

    update for all html file

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__FILE__));
-$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrin($_SERVER['PATH_INFO'], '/')) : [];
-require_once(ROOT . DS . 'layout' . DS . 'header.php');
-require_once(ROOT . DS . 'view' . DS . 'index.php');
-require_once(ROOT . DS . 'layout' . DS . 'footer.php');
-?>
